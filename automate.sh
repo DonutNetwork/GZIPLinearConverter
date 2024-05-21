e0f262267e54789de9c876c110511f494b7cde8f
Pedro Paulo Monte Pagani <56891617+PedroMPagani@users.noreply.github.com>
2024-05-21 13:08:17 -0300

    Create automate.sh

---
+++ automate.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/DonutNetwork/GZIPLinearConverter.git
+mv GZIPLinearConverter/* .
+rm -R GZIPLinearConverter
+apt install python3-pip
+pip3 install -r requirements.txt
+chmod +x convert_region_files.py
+mv ./world/region ./world/oldformat
+mkdir ./world/region
+./convert_region_files.py mca2linear ./world/oldformat/ ./world/region --threads 256
